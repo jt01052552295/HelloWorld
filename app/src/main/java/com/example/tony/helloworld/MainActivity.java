5e6edbfe53a5dc3ca8a933f9ba12d9e2cc491165
jt01052552295 <jt01052552295@gmail.com>
2017-05-23 21:19:19 +0900

    Update MainActivity.java
    주석 추가

--- app/src/main/java/com/example/tony/helloworld/MainActivity.java
+++ app/src/main/java/com/example/tony/helloworld/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e(TAG, "로그 검사");
+        Log.e(TAG, "로그 검사"); // 주석
     }
 }
